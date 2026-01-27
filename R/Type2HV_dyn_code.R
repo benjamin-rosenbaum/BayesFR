01a0581b7079bcb4c046edd2b6785d7e7224d45a
Benjamin Rosenbaum <benjamin.rosenbaum@gmx.de>
2026-01-27 11:18:34 +0100

    Hassll-Varley model add

---
+++ R/Type2HV_dyn_code.R
@@ -0,0 +1,27 @@
+#' Functional response models with predator interference
+#'
+#' Contains Stan function of the same name as character string. Uses analytical
+#' solution of the \bold{Hassell-Varley} model
+#' \deqn{\frac{dN}{dt}=-\frac{aN}{P^c+ahN}P}{dN/dt=-aNP/(P^c+ahN)}
+#' to compute number of eaten prey. Rogers random predator equation with LambertW
+#' function is used with modified attack rates. Predator interference affects attack
+#' rates only.
+#'
+#' Usage in brms formula:\cr\cr
+#' \verb{~ Type2HV_dyn(N, P, Time, a, h, c)}\cr\cr
+#' \verb{N    } initial number of prey \cr
+#' \verb{P    } number of predators \cr
+#' \verb{Time } duration of the experiment \cr
+#' \verb{a    } attack rate for P=1 \cr
+#' \verb{h    } handling time \cr
+#' \verb{c    } predator interference coefficient
+
+#' @return Number of eaten prey
+#' @export
+Type2HV_dyn_code = "
+// prediction
+real Type2HV_dyn(real N, real P, real Time, real a, real h, real c){
+  real a_new = a/(P^c);
+  return(N-lambert_w0(a_new*h*N*exp(a_new*(h*N-P*Time)))/(a_new*h));
+}
+"
